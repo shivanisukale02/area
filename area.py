855aa580490d6deac04d2ccc266c8b0b18a53f30
Shivani Sukale <sukaleshivani@gmail.com>
2022-03-28 16:06:39 +0530

    menu deiven test cases

--- area.py
+++ area.py
@@ -8,24 +8,26 @@ def areaSquare(x):
     return x*x
 
 if __name__=="__main__":
-    n1=int(input("Enter value1"))
-    n2=int(input("Enter value2"))
-    print(arearec(n1,n2))
-    print(periRec(n1,n2))
-    print(areaSquare(n1))
 
-while(True):
-    print('''
-          1. Area of rectangle
-          2. Perimeter of Rectangle
-          3. Area of Square
-          ''')
-    ch = int(input("enter your choice"))
-    if ch == 1:
-        print(arearec(n1, n2))
-    elif ch == 2:
-        print(periRec(n1, n2))
-    elif ch == 3:
-        print(areaSquare(n1))
-    else:
-        print("Invalid choice")
+    while(True):
+        print('''
+              1. Area of rectangle
+              2. Perimeter of Rectangle
+              3. Area of Square
+              ''')
+        ch = int(input("enter your choice"))
+
+        if ch == 1:
+            n1 = int(input("Enter value1"))
+            n2 = int(input("Enter value2"))
+            print(arearec(n1, n2))
+        elif ch == 2:
+            n1 = int(input("Enter value1"))
+            n2 = int(input("Enter value2"))
+            print(periRec(n1, n2))
+        elif ch == 3:
+            n1 = int(input("Enter value1"))
+            print(areaSquare(n1))
+        else:
+            print("Invalid choice")
+            break
